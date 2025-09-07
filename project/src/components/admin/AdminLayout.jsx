3b7b486e7cc38549a3bf7c44891c3da64e0df5ad
Jayashree Patra <153646154+Jayashre2001@users.noreply.github.com>
2025-09-07 11:19:42 +0530

    change admin layout content

--- project/src/components/admin/AdminLayout.jsx
+++ project/src/components/admin/AdminLayout.jsx
@@ -381,16 +381,7 @@ const getAdminMenuItems = () => [
     bgColor: "bg-purple-50",
     textColor: "text-purple-700",
   },
-  { 
-    name: "Classes & Subjects", 
-    path: "/admin/classes", 
-    icon: BookOpen, 
-    color: "from-amber-500 to-amber-600",
-    hoverColor: "hover:from-amber-600 hover:to-amber-700",
-    iconColor: "text-amber-100",
-    bgColor: "bg-amber-50",
-    textColor: "text-amber-700"
-  },
+
   { 
     name: "Attendance", 
     path: "/admin/attendance", 
